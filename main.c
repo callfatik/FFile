837b931ab20e5e3d99ece8835a5c7afb2c9f66c9
callfatik <ambreenijaz097@gmail.com>
2025-01-19 09:01:08 +0500

    2.0

--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+#include <dirent.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -12,12 +13,33 @@ void help() {
   printf("\nexit -- Used to quit");
 }
 
-void make_file(char arg[])
-{
-    FILE *f;
-    f = fopen(arg,"w");
-    fclose(f);
-    free(f);
+void make_file(char arg[]) {
+  FILE *f;
+  f = fopen(arg, "w");
+  fclose(f);
+}
+
+void remove_file(char arg[]) { remove(arg); }
+
+void list_files() {
+  DIR *dir = opendir(".");
+  struct dirent *entity;
+  printf("\n");
+  while ((entity = readdir(dir)) != NULL) {
+    printf("%s\n", entity->d_name);
+  }
+  closedir(dir);
+}
+
+void find_file(char arg[]) {
+  DIR *dir = opendir(".");
+  struct dirent *entity;
+  printf("\n");
+  while ((entity = readdir(dir)) != NULL) {
+    while ((strcmp(arg, entity->d_name)) != 0)
+      printf("Found = %s\n", entity->d_name);
+  }
+  closedir(dir);
 }
 
 int main() {
@@ -37,25 +59,27 @@ int main() {
     // input[strspn(input, "\n")] = '\0';
 
     // Parsing input
-    sscanf(input, "%[a-zA-z] %[a-zA-Z]", command, arg);
+    sscanf(input, "%[a-zA-Z.] %[a-zA-Z.]", command, arg);
 
     // If Conditions
     if ((strcmp(command, "touch")) == 0) {
-            make_file(arg);
-            printf("m!");
+      make_file(arg);
+      printf("FILE MADE!");
     } else if ((strcmp(command, "remove")) == 0) {
-      printf("remove!");
+      remove_file(arg);
+      printf("removed!");
     } else if ((strcmp(command, "list")) == 0) {
+      list_files();
       printf("l!");
     } else if ((strcmp(command, "find")) == 0) {
+      find_file(arg);
       printf("f!");
     } else if ((strcmp(command, "exit")) == 0) {
       printf("Exiting...");
       break;
     } else if ((strcmp(command, "Help")) == 0) {
       help();
-    }
-    else {
+    } else {
       printf("Invalid Try Again. Type \"Help\" if you need Command List");
     }
   }
